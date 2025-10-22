bb6ce5eae63329a4660385b9deaf0d39797ec691
Ohad Bitton <ohadbitton1@gmail.com>
2025-10-22 14:55:43 +0300

    feat: Complete and test __getitem__ in SQLDataset

--- src/dataset.py
+++ src/dataset.py
@@ -10,3 +10,18 @@ class SQLDataset (Dataset):
 
     def __len__(self):
         return len(self.dataset)
+    
+    def __getitem__(self, index):
+        question =  self.dataset['question'][index]
+        query = self.dataset['sql'][index]
+        T5_prefix = "translate English to SQL: "
+        full_request = T5_prefix + question
+        source_incoding = self.tokenizer(full_request, max_length= self.max_length, truncation=True, padding= 'max_length', return_tensors='pt')
+        target_incoding = self.tokenizer(query, max_length= self.max_length, truncation=True, padding= 'max_length', return_tensors='pt')
+        
+        dict_data = {
+        'input_ids': source_incoding['input_ids'].squeeze(),
+        'labels': target_incoding['input_ids'].squeeze(),
+        'attention_mask': source_incoding['attention_mask'].squeeze()}
+
+        return dict_data
