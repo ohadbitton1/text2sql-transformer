cb3020e31c83dd3ef01b57ed208b66d688654721
Ohad Bitton <ohadbitton1@gmail.com>
2025-10-27 16:19:07 +0200

    moving to g.colab for gpu running

--- src/train.py
+++ src/train.py
@@ -5,7 +5,9 @@ from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, get_linear_schedu
 from src.data_processing import get_dataloaders
 
 def main():
-    
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device}")
+
     parser = argparse.ArgumentParser(description="Fine-tune a T5 model for Text-to-SQL.")
     parser.add_argument("--learning_rate", type=float, default=0.0001, help="The initial learning rate.")
     parser.add_argument("--num_epochs", type=int, default=3, help="Total number of training epochs.")
@@ -17,11 +19,12 @@ def main():
     tokenizer = AutoTokenizer.from_pretrained(args.model_name)
     print(f"Placeholder for loading tokenizer for model: {args.model_name}")
     model = AutoModelForSeq2SeqLM.from_pretrained(args.model_name)
+    model.to(device)
     print("Placeholder for loading pre-trained model.")
 
     max_token_length = tokenizer.model_max_length
 
-    train_dataloader, val_dataloader = get_dataloaders('../data/train.csv','../data/validation.csv', args.batch_size ,tokenizer, max_token_length )
+    train_dataloader, val_dataloader = get_dataloaders('data/train.csv','data/validation.csv', args.batch_size ,tokenizer, max_token_length )
     print("Placeholder for creating DataLoaders.")
     
     total_training_steps = len(train_dataloader) * args.num_epochs
@@ -30,6 +33,7 @@ def main():
     scheduler = get_linear_schedule_with_warmup(optimizer,warmup_steps,total_training_steps )
     print("\nModel and data loaded successfully. Ready for training.")
 
+    
 
     #EPOCH LOOP
 
@@ -37,23 +41,30 @@ def main():
         model.train()
         total_val_loss= 0
         for i, batch in enumerate(train_dataloader):
+            for key, value in batch.items():
+                batch[key] = value.to(device)
+            optimizer.zero_grad()    
             outputs= model(**batch)
             loss = outputs.loss
             loss.backward()
             optimizer.step()
             scheduler.step()
-            optimizer.zero_grad()
+            
             if (i + 1) % 50 == 0:
                 print(f"Epoch {epoch+1}, Step {i+1}, Train Loss: {loss.item()}")
+    
         model.eval()
         with torch.no_grad():
             for batch in val_dataloader:
+                for key, value in batch.items():
+                    batch[key] = value.to(device)
                 outputs = model(**batch)
                 loss = outputs.loss
                 total_val_loss += loss.item()
 
         avg_val_loss = total_val_loss/len(val_dataloader)
         print(f"Epoch {epoch+1}/{args.num_epochs}, Validation Loss:{avg_val_loss}")
+
 
 
 
